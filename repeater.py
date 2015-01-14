1bae72aeb5ca960edbdd983dbf9ef4e0090a6f50
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-01-14 09:30:48 +0000

    make it work

--- repeater.py
+++ repeater.py
@@ -3,22 +3,25 @@ import serial, time
 upstreamName = '/dev/ttyO2'
 downstreamName = '/dev/ttyO1'
 serialConfig = {
-        'baudrate': 38400,
-        'bytesize': serial.EIGHTBITS,
-        'parity': serial.PARITY_NONE,
-        'stopbits': serial.STOPBITS_ONE,
-        'timeout': 1
-        }
+    'baudrate': 38400,
+    'bytesize': serial.EIGHTBITS,
+    'parity': serial.PARITY_NONE,
+    'stopbits': serial.STOPBITS_ONE,
+    'timeout': 0.1
+    }
 upstream = serial.Serial(upstreamName, **serialConfig)
 downstream = serial.Serial(downstreamName, **serialConfig)
 
-def printByteCount(c, direction):
-    if c > 0:
-        print("wrote "+ str(c) +" bytes "+direction)
+def repeat(fromLabel, fromPort, toLabel, toPort):
+  data = fromPort.readall()
+  length = len(data)
+  if ( length > 0 ):
+    print("repeating "+str(length)+" bytes from "+fromLabel+" into "+toLabel)
+    hexstr = ':'.join(x.encode('hex') for x in data)
+    print(hexstr)
+    toPort.write(data);
 
 
 while upstream.isOpen() and downstream.isOpen():
-    ds = downstream.write(upstream.read())
-    us = upstream.write(downstream.read())
-    printByteCount(ds, 'downstream')
-    printByteCount(us, 'upstream')
+  repeat('pc', upstream, 'printer', downstream)
+  repeat('printer', downstream, 'pc', upstream)
